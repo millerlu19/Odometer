beb8bb54517e3bc0e883c15eb8791d8f4286826a
Lucas Miller <millerlu19@hanover.edu>
2018-05-18 11:29:29 -0400

    Added initial code to OdometerService

--- app/src/main/java/edu/hanover/odometer/OdometerService.java
+++ app/src/main/java/edu/hanover/odometer/OdometerService.java
@@ -2,15 +2,21 @@ package edu.hanover.odometer;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class OdometerService extends Service {
-    public OdometerService() {
+
+    private final IBinder binder = new OdometerBinder();
+
+    public class OdometerBinder extends Binder {
+        OdometerService getOdometer() {
+            return OdometerService.this;
+        }
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return binder;
     }
 }
